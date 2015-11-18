d6317868c6ac93812ba9c8aeb76f032a5f390419
dinithminura <dinithmsdk0007@gmail.com>
2015-11-18 17:26:00 +0530

    UI added to balance query and balance result

---
+++ mbank/src/main/java/com/wasn/activities/BalanceQueryActivity.java
@@ -0,0 +1,56 @@
+package com.wasn.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.wasn.application.MobileBankApplication;
+
+/**
+ * Created by root on 11/18/15.
+ */
+public class BalanceQueryActivity extends Activity implements View.OnClickListener{
+    MobileBankApplication application;
+    RelativeLayout back;
+    RelativeLayout done;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.balance_query_layout);
+        init();
+    }
+
+    public void init(){
+        application = (MobileBankApplication) BalanceQueryActivity.this.getApplication();
+        back = (RelativeLayout) findViewById(R.id.balance_query_layout_back);
+        done = (RelativeLayout) findViewById(R.id.balance_query_layout_get_balance);
+
+        back.setOnClickListener(BalanceQueryActivity.this);
+        done.setOnClickListener(BalanceQueryActivity.this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == back){
+            startActivity(new Intent(BalanceQueryActivity.this, MobileBankActivity.class));
+            BalanceQueryActivity.this.finish();
+            application.resetFields();
+        }
+        if(view == done){
+            startActivity(new Intent(BalanceQueryActivity.this,BalanceResultActivity.class));
+            BalanceQueryActivity.this.finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // back to main activity
+        startActivity(new Intent(BalanceQueryActivity.this, MobileBankActivity.class));
+        BalanceQueryActivity.this.finish();
+        application.resetFields();
+    }
+}
---
+++ mbank/src/main/java/com/wasn/activities/BalanceResultActivity.java
@@ -0,0 +1,69 @@
+package com.wasn.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import com.wasn.application.MobileBankApplication;
+import com.wasn.pojos.Attribute;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by root on 11/18/15.
+ */
+public class BalanceResultActivity extends Activity implements View.OnClickListener {
+    MobileBankApplication application;
+
+    ListView balanceList;
+    ArrayList<Attribute> attributesList;
+    AttributeListAdapter adapter;
+    RelativeLayout back;
+
+    //to populate list
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.balance_query_result_layout);
+        init();
+    }
+
+    public void init() {
+
+        back = (RelativeLayout) findViewById(R.id.balance_query_result_layout_back);
+        back.setOnClickListener(BalanceResultActivity.this);
+
+        //Dummy data added
+        attributesList = new ArrayList<>();
+        attributesList.add(new Attribute("Acc No: ", "123"));
+        attributesList.add(new Attribute("Customer: ", "FirstName_LastName"));
+        attributesList.add(new Attribute("NIC: ","000000000v"));
+        attributesList.add(new Attribute("Balance: ","15,000.00"));
+
+        balanceList = (ListView) findViewById(R.id.balance_result_list);
+        adapter = new AttributeListAdapter(BalanceResultActivity.this, attributesList);
+        balanceList.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == back) {
+            //go back to BalanceQueryActivity
+            startActivity(new Intent(BalanceResultActivity.this, BalanceQueryActivity.class));
+            BalanceResultActivity.this.finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // back to ClientListActivity
+        startActivity(new Intent(BalanceResultActivity.this, BalanceQueryActivity.class));
+        BalanceResultActivity.this.finish();
+    }
+
+}
